520583973c1a6eec28aecbf681750db628bce421
joseph_ctu <sephxd1234@gmail.com>
2018-02-20 21:32:18 +0100

    feat: add auth methods

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -1,8 +1,11 @@
+/*eslint no-console: "off" */
 import auth0 from "auth0-js";
+import history from "../history";
 
 export default class Auth {
   auth0 = new auth0.WebAuth({
     domain: "icjoseph.eu.auth0.com",
+    /*eslint-disable-next-line*/
     clientID: process.env.REACT_APP_CLIENT_ID,
     redirectUri: "http://localhost:3000/callback",
     audience: "https://icjoseph.eu.auth0.com/userinfo",
@@ -10,7 +13,41 @@ export default class Auth {
     scope: "openid"
   });
 
-  login() {
+  handleAuthentication = () => {
+    this.auth0.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        this.setSession(authResult);
+        history.replace("/home");
+      } else if (err) {
+        history.replace("/home");
+        console.log(err);
+      }
+    });
+  };
+
+  setSession = authResult => {
+    let expiresAt = JSON.stringify(
+      authResult.expiresIn * 1000 + new Date().getTime()
+    );
+    localStorage.setItem("access_token", authResult.accessToken);
+    localStorage.setItem("id_token", authResult.idToken);
+    localStorage.setItem("expires_at", expiresAt);
+    history.replace("home");
+  };
+
+  login = () => {
     this.auth0.authorize();
-  }
+  };
+
+  logout = () => {
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("id_token");
+    localStorage.removeItem("expires_at");
+    history.replace("/home");
+  };
+
+  isAuthenticate = () => {
+    let expiresAt = JSON.parse(localStorage.getItem);
+    return new Date().getTime() < expiresAt;
+  };
 }
