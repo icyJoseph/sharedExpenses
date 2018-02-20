fcde2ea2dcc796cc064dc06fa344631dd63fcc94
joseph_ctu <sephxd1234@gmail.com>
2018-02-20 16:17:51 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 data-heading="Click to Login">Click to Login</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
