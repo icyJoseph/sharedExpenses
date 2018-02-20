81d596f51afbb4fb2bcf65953394f55a9abf239e
joseph_ctu <sephxd1234@gmail.com>
2018-02-20 21:31:56 +0100

    feat: allow basic log in

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class App extends Component {
+  login = () => {
+    this.props.auth.login();
+  };
+
+  logout = () => {
+    this.props.auth.logout();
+  };
+
+  loginScreen = msg => (
+    <h1 onClick={this.login} data-heading={msg}>
+      {msg}
+    </h1>
+  );
+
+  loggedIn = msg => <h1 data-heading={msg}>{msg}</h1>;
+
   render() {
+    const { isAuthenticated } = this.props.auth;
+    const msg = "Click to Login";
+    const msg2 = "Logged in";
     return (
       <div className="container">
-        <h1 data-heading="Click to Login">Click to Login</h1>
+        {isAuthenticated ? this.loggedIn(msg2) : this.loginScreen(msg)}
       </div>
     );
   }
 }
 
 export default App;
+
+App.propTypes = {
+  auth: PropTypes.object
+};
