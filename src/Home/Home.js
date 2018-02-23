6b5680ebc53e929638a4726ef912886788b9807c
joseph_ctu <sephxd1234@gmail.com>
2018-02-23 18:10:49 +0100

    feat: Load spinner and show data when fetch is complete

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,19 +1,48 @@
-import React from "react";
-import Profile from "../Profile/Profile";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
+import Spinner from "react-spinkit";
+import Profile from "../Profile/Profile";
+import Table from "../Table/Table";
+import * as api from "../Api/Api";
 
-const Home = ({ auth }) => {
-  /*eslint-disable-next-line*/
-  console.log(auth);
-  return (
-    <div style={styles.container}>
-      <div style={styles.title}>
-        <h2 style={{ color: "#000" }}>Welcome!</h2>
+class Home extends Component {
+  state = {
+    data: [],
+    spinner: true
+  };
+
+  componentDidMount() {
+    const loading = setTimeout(this.getData, 1000);
+    return loading;
+  }
+
+  getData = () => {
+    const { auth: { userProfile: { sub } } } = this.props;
+    api
+      .columns({ user_id: sub })
+      .then(res => this.setState({ data: res.values, spinner: false }));
+  };
+
+  render() {
+    const { auth } = this.props;
+    const { data, spinner } = this.state;
+    return (
+      <div style={styles.container}>
+        <div style={styles.title}>
+          <h2 style={{ color: "#000" }}>Welcome!</h2>
+        </div>
+        <div style={styles.content}>
+          {spinner ? (
+            <Spinner name="ball-pulse-rise" color="black" />
+          ) : (
+            <Table data={data} />
+          )}
+        </div>
+        <Profile auth={auth} style={styles.profile} />
       </div>
-      <Profile auth={auth} style={styles.profile} />
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Home;
 
@@ -33,7 +62,7 @@ const styles = {
     flexDirection: "column",
     flex: 1,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "flex-start"
   },
   profile: {
     display: "flex",
@@ -41,5 +70,12 @@ const styles = {
     flex: 1,
     alignItems: "center",
     justifyContent: "center"
+  },
+  content: {
+    display: "flex",
+    flexDirection: "column",
+    flex: 2,
+    alignItems: "center",
+    justifyContent: "flex-start"
   }
 };
