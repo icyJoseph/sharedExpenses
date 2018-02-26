3cccfa72b97ab066efc04acf96e9ae3af90328bc
joseph_ctu <sephxd1234@gmail.com>
2018-02-26 22:57:24 +0100

    feat: fetch from new endpoint

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,25 +1,18 @@
 import axios from "axios";
 
-const googleapis = "https://sheets.googleapis.com/v4/spreadsheets";
-
-export const columns = ({ user_id }) => {
-  const api_url = `${googleapis}/${
-    process.env.REACT_APP_SPREADSHEET_KEY
-  }/values/Sheet1?majorDimension=ROWS`;
-  const token = process.env.REACT_APP_JWT;
+export const rows = () => {
+  const apiEndPoint = process.env.REACT_APP_WEB_TASK_GOOGLE_SPREADSHEET;
   return axios
     .post(
-      process.env.REACT_APP_WEB_TASK_END_POINT,
-      {
-        api_url,
-        user_id
-      },
+      apiEndPoint,
+      {},
       {
         headers: {
-          Authorization: `Bearer ${token}`,
           "Content-Type": "application/json"
         }
       }
     )
-    .then(res => res.data.data);
+    .then(res => {
+      return res.data;
+    });
 };
