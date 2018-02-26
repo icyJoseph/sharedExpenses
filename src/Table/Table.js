ab56fa6c68d35b1ca3c8f787b3c51099cc45b96f
joseph_ctu <sephxd1234@gmail.com>
2018-02-26 22:59:37 +0100

    feat: use react-list component

--- src/Table/Table.js
+++ src/Table/Table.js
@@ -1,23 +1,44 @@
-import React, { Fragment } from "react";
+import React, { Component, Fragment } from "react";
+import ReactList from "react-list";
 import PropTypes from "prop-types";
-import { objectBuilder } from "./Utils";
+import Row from "./Row";
 
-const Table = ({ data }) => {
-  const entries = objectBuilder(data);
-  return (
-    <Fragment>
-      <div>Yay! We have data!</div>
-      <ul style={{ listStyle: "none" }}>
-        {entries.map(entry => (
-          <li key={entry.id}>
-            {entry.description} {entry.amount} {entry.status}
-          </li>
-        ))}
-      </ul>
-    </Fragment>
+class Table extends Component {
+  state = {
+    data: []
+  };
+
+  componentDidMount() {
+    this.setState({ data: this.props.data });
+  }
+
+  renderItem = (index, key) => (
+    <Row
+      key={key}
+      style={{ height: 30 }}
+      data={this.state.data[index]}
+      index={index}
+    />
   );
-};
 
+  render() {
+    const { data } = this.state;
+    return (
+      <Fragment>
+        <div style={{ margin: 10 }}>Yay! We have data!</div>
+        <div
+          style={{ overflow: "auto", maxHeight: 200, width: "70%", margin: 5 }}
+        >
+          <ReactList
+            itemRenderer={this.renderItem}
+            length={data.length}
+            type="uniform"
+          />
+        </div>
+      </Fragment>
+    );
+  }
+}
 export default Table;
 
 Table.propTypes = {
