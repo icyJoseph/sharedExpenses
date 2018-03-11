c7d53aec61df1736380c72728094fd045c03ec66
joseph_ctu <sephxd1234@gmail.com>
2018-03-11 17:20:16 +0100

    feat: get user meta data

--- src/Api/index.js
+++ src/Api/index.js
@@ -30,3 +30,25 @@ export const addRow = (token, row) => {
     )
     .then(res => res.data);
 };
+
+export const getUserMetaData = sub => {
+  return axios
+    .post(
+      process.env.REACT_APP_WEB_TASK_JWT,
+      { app_name: "shared" },
+      { headers: { "Content-Type": "application/json" } }
+    )
+    .then(res => res.data.result)
+    .then(JSON.parse)
+    .then(res => res.access_token)
+    .then(token =>
+      axios
+        .get(`${process.env.REACT_APP_USER_END_POINT}${sub}`, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+          }
+        })
+        .then(res => res.data.user_metadata)
+    );
+};
