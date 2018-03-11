629b2c7724355331fd1c18ee5a9e658fb8e97166
joseph_ctu <sephxd1234@gmail.com>
2018-03-11 19:19:54 +0100

    feat: add function helpers to calculate debt

--- src/Components/Table/Utils/index.js
+++ src/Components/Table/Utils/index.js
@@ -23,3 +23,27 @@ export const objectBuilder = data => {
 export const capitalizer = text => {
   return text.charAt(0).toUpperCase() + text.slice(1);
 };
+
+// TODO: filter what has been already paid
+export const debtCalculator = data => {
+  const individual = individualDebt(data);
+  const total = totalDebt(data);
+  return { individual, total };
+};
+
+const individualDebt = data => {
+  const owners = data.map(e => e.owner);
+  const unique = [...new Set(owners)];
+  return unique.map(owner => {
+    return {
+      owner,
+      total: data
+        .filter(e => e.owner === owner)
+        .reduce((acc, val) => acc + parseFloat(val.amount), 0)
+    };
+  });
+};
+
+const totalDebt = data => {
+  return data.reduce((acc, val) => acc + parseFloat(val.amount), 0);
+};
